6998d431f9833aca71194363f8c09ede5e0e1c85
TateTheBait <123601564+TateTheBait@users.noreply.github.com>
2024-05-10 17:23:28 -0500

    Update client.lua

--- AntiLagg/client.lua
+++ AntiLagg/client.lua
@@ -1,32 +1,5 @@
 local usesound = false
 
-function CreateVehicleExhaustBackfire(vehicle, scale)
-    local exhaustNames = {
-      "exhaust",    "exhaust_2",  "exhaust_3",  "exhaust_4",
-      "exhaust_5",  "exhaust_6",  "exhaust_7",  "exhaust_8",
-      "exhaust_9",  "exhaust_10", "exhaust_11", "exhaust_12",
-      "exhaust_13", "exhaust_14", "exhaust_15", "exhaust_16"
-    }
-  
-    for _, exhaustName in ipairs(exhaustNames) do
-      local boneIndex = GetEntityBoneIndexByName(vehicle, exhaustName)
-  
-      if boneIndex ~= -1 then
-        local pos = GetWorldPositionOfEntityBone(vehicle, boneIndex)
-        local off = GetOffsetFromEntityGivenWorldCoords(vehicle, pos.x, pos.y, pos.z)
-  
-        UseParticleFxAssetNextCall('core')
-        StartParticleFxNonLoopedOnEntity('veh_backfire', vehicle, off.x, off.y, off.z, 0.0, 0.0, 0.0, scale, false, false, false)
-      end
-    end
-  end
-
-  local function vehicleActivate(veh, setType, value)
-    if not veh or not DoesEntityExist(veh) then return end
-    local state = Entity(veh).state
-    state:set(("nd_nitro_activated_%s"):format(setType), value, true)
-end
-
 local isondelay = false
 local function pop()
   Citizen.CreateThread(function ()
